afdc1e3399f550b1a9df81bef9c087b49da835c4
Tigran Vardanyan <tigran.vardanyan@caiman.am>
2023-05-17 00:20:48 +0400

    create test server

--- functions/index.js
+++ functions/index.js
@@ -20,43 +20,17 @@
 import fs from 'node:fs';
 import ytdl from 'ytdl-core';
 import http from 'http';
-import https from 'https';
 
-// Intercept HTTP requests
-const oldHttp = http.request;
-http.request = function (options, callback) {
-  console.log('HTTP request:', options.hostname + options.path);
-  return oldHttp.apply(this, arguments);
-};
+const server = http.createServer((req, res) => {
+  console.log('starting')
+});
+server.listen(8080, () => {
+  console.log('starting')
+});
 
-// Intercept HTTPS requests
-const oldHttps = https.request;
-https.request = function (options, callback) {
-  console.log('HTTPS request:', options.hostname + options.path);
-  return oldHttps.apply(this, arguments);
-};
 // TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
 // TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
 // TypeScript: import ytdl = require('ytdl-core'); with neither of the above
-
-// [
-//   'page',
-//   'player_response',
-//   'response',
-//   'html5player',
-//   'formats',
-//   'related_videos',
-//   'videoDetails',
-//   'full'
-// ]
-
-const oldPackageMethod = ytdl.getBasicInfo;
-ytdl.getBasicInfo = function () {
-  // Get the current call stack
-  const stack = new Error().stack.split('\n').slice(2).join('\n');
-  console.log(`Calling myPackage.method\nCall Stack:\n${stack}`);
-  return oldPackageMethod.apply(this, arguments);
-};
 
 // Call the package method to test
 // myPackage.method();
