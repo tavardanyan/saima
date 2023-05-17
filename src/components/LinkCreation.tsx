2366dead1e93155fb28a51a1f2bd068ac4e38e72
Tigran Vardanyan <tigran.vardanyan@caiman.am>
2023-05-17 09:45:14 +0400

    request api to get new key

--- src/components/LinkCreation.tsx
+++ src/components/LinkCreation.tsx
@@ -1,28 +1,57 @@
-import { useState, FormEvent } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
+type SuccessData = {
+  id: string;
+}
+
+type ErrorResponse = {
+  where: string;
+  message: string;
+}
+
+type Response = {
+  status: number;
+  message: string;
+  data: SuccessData | null;
+  errors: ErrorResponse[] | null;
+}
 
 function LinkCreation() {
 
-  const [name, setName] = useState<string>("");
+  const [url, setUrl] = useState<string>("");
+  const [link, setLink] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    alert(`The name you entered was: ${name}`)
+  const handleSubmit = async () => {
+    const apiUrl: string = process.env.REACT_APP_API_URL as string;
+    const response = await axios.post<Response>(apiUrl, { url });
+    if (response.data.errors) {
+      console.log(response.data)
+      const msg = response.data.errors.map((err: ErrorResponse) => `${err.where}: ${err.message}`).join('\n');
+      setMessage(msg);
+    } else {
+      setMessage(response.data.message);
+      console.log(response.data)
+      if (response.data.data) {
+        setLink(window.location.href + response.data.data.id);
+      }
+    }
   }
 
   return (
     <div className="LinkCreation">
       <h1>LinkCreation</h1>
-      <p>{name}</p>
-      <form onSubmit={handleSubmit} noValidate>
-        <label>Enter your Video URL:
-          <input 
-            type="text" 
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <input type="submit" />
-    </form>
+      <p>{message}</p>
+      <label>Enter your Video URL:
+        <input 
+          type="text" 
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+      </label>
+      <button onClick={handleSubmit}>Create Link</button>
+      <p>{link}</p>
     </div>
   );
 }
